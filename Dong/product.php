2b0ddf910dafa57fe04ea4d400b01cd0429ddd56
Kanengovi <dong.ngovi@gmail.com>
2025-12-01 16:03:54 +0700

    product

--- Dong/product.php
+++ Dong/product.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . "./database/config.php";
-require_once __DIR__ . "./database/dbhelper.php";
+require_once("../Dong/database/config.php");
+require_once("../Dong/database/dbhelper.php");
 
 // Get filter parameters
 $category_filter = isset($_GET['category']) ? $_GET['category'] : [];
