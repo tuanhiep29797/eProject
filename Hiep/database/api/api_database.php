d5ebc7b3c2db6de52c26f4aca7d0e94f2c3df5a6
tuanhiep29797 <tuanhiep29797@gmail.com>
2025-12-02 19:30:19 +0700

    update api

---
+++ Hiep/database/api/api_database.php
@@ -0,0 +1,8 @@
+<?php
+    require_once("../database/dbhelper.php");
+    require_once("api_category.php");
+    require_once("api_brand.php");
+    require_once("api_product.php");
+    require_once("api_user.php");
+    require_once("api_user_user.php");
+?>
--- Hiep/database/upload/upload_database.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("../database/dbhelper.php");
-    require_once("upload_category.php");
-    require_once("upload_brand.php");
-    require_once("upload_product.php");
-    require_once("upload_admin.php");
-?>
