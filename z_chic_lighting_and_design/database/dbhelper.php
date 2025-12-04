0fb48e0cf441964c447ce5913bb307295e680651
tuanhiep29797 <tuanhiep29797@gmail.com>
2025-12-05 00:45:48 +0700

    db

--- z_chic_lighting_and_design/database/dbhelper.php
+++ z_chic_lighting_and_design/database/dbhelper.php
@@ -113,7 +113,7 @@
 		order_id INT AUTO_INCREMENT PRIMARY KEY,
 		user_id INT NOT NULL,
 		order_status ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled') NOT NULL DEFAULT 'pending',
-		total_amount DECIMAL(10, 2) NOT NULL,
+		total_amount DECIMAL(15, 2) NOT NULL,
 		receiver VARCHAR(255) NOT NULL,
 		address VARCHAR(255) NOT NULL,
 		order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
