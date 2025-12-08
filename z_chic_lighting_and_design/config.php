f33852aa14b6fb3e50afdc24c0fca7ec1787c352
KaiSser <tuanhiep29797@gmail.com>
2025-12-08 18:09:26 +0700

    config

--- z_chic_lighting_and_design/config.php
+++ z_chic_lighting_and_design/config.php
@@ -8,7 +8,6 @@
     define("USERNAME", "root");
     define("PASSWORD", "");
 
-    // define("BASE_URL", "http://localhost/PHP/eProject/z_chic_lighting_and_design/");
     define("BASE_URL", "http://localhost/eProject/z_chic_lighting_and_design/");
 
     //function check Login
