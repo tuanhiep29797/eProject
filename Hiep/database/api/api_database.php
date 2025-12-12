af7d5ffe09e01c8854ec0b31266be50199e3709c
KaiSser <tuanhiep29797@gmail.com>
2025-12-12 18:37:46 +0700

    delete st

--- Hiep/database/api/api_database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../database/dbhelper.php");
-    require_once("api_category.php");
-    require_once("api_brand.php");
-    require_once("api_product.php");
-    require_once("api_user.php");
-    require_once("api_user_user.php");
-    require_once("api_feedback.php");
-?>
--- Hiep/database/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once('dbhelper.php');
-    require_once('init.php');
-    require_once('upload/upload_database.php');
-?>
